5c71fcc2c79b92a22b9263e84401fe7b52de286f
Hannah Brantley <hannahbrantley@Hannahs-MacBook-Pro.local>
2020-05-06 16:41:06 -0600

    works and some added style and functionality

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $('#addskill').click(function() {
     var $newSkill = `<tr>
-                    <td><button class="remove">X</button></td>
-                    <td>${$('input').val()}</td>
-                    </tr>`
-    $('table').append($newSkill);
+                    <td><button class="remove">X</button>${$('input').val()}</td>
+                    </tr>`;
+    $($newSkill).insertBefore($('.last'));
 
     $('.remove').click(function() {
-        console.log('x marks the spot');
         $(this).closest('tr').remove();
-        console.log($(this));
     })
+
+    $('input').val('');
+
 });
